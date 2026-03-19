c69c25928ea8dfc2d2261e4ceca92b34a213c954
Vander <129402129+MartinsVander@users.noreply.github.com>
2026-03-19 19:35:30 -0300

    v 1.0

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+function  calculoImc(){
+    let peso = parseFloat(document.querySelector("#peso").value);
+    let altura = parseFloat(document.querySelector("#altura").value);
+    let imc =peso/(altura*altura);
+    document.querySelector("#saida").textContent =imc.toFixed(2);
+}
